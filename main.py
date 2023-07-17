753fb8fb1d69b6ea23999c44be188fe9b5a7c68f
satvik <satvik.jain.kht@gmail.com>
2023-07-17 23:43:14 +0530

    extracted colors

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
 import colorgram
 
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
 for color in colors:
-    rgb_colors.append(color.rgb)
-
+    color_list = []
+    for _ in color.rgb:
+        color_list.append(_)
+    rgb_colors.append(tuple(color_list))
 print(rgb_colors)
+
